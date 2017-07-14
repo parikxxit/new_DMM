5caabd2ecc1d2d045bc72fde11bcdb906ab7b1ed
Parikshit Singh <singhp154154@gmail.com>
2017-07-14 21:24:37 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Innovation Group</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--BOOTSTRAP
+		<link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">-->
+	</head>
+	<body>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span class="icon fa-map-marker"></span>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1><strong>Innovation Group</strong></h1>
+								<p>Project DMM (digital map of MANIT/NITB) </p>
+							</div>
+						</div>
+						<nav>
+							<ul>
+								<li><a href="terms&cond.php">Explore Digital MANIT/NIT-B!</a></li>
+								</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Login Signup -->
+							<article id="intro">
+								<ul class="icons">
+								<li><a href="https://www.facebook.com/Dmm005/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a href="https://www.youtube.com/channel/UCq-cBh41uvDZ7ahYlmhMN8A" class="icon fa-youtube"><span class="label">Youtube</span></a></li>		
+							    </ul>
+							</article>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">&copy; NitinMi210 Tech. All rights reserved</p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+		<!--Ajaxing the map-->
+			<script src="assets/js/ajaxmap.js"></script>
+</body>
+</html>
