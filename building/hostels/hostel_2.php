8ef1886a3ce316b3053e2a0118dbde8d48889e90
Parikshit Singh <singhp154154@gmail.com>
2017-07-14 21:02:53 +0000

    Add files via upload

---
+++ building/hostels/hostel_2.php
@@ -0,0 +1,44 @@
+<?php
+	$title = "Hostel 2";
+	$download = "hostel_2.kmz";
+ ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	Head Tag
+	<?php
+		require '../head.php';
+	 ?>
+	<body>
+
+	<header id="fh5co-header" role="banner">
+		<?php
+			require '../../navigation.php';
+		 ?>
+	</header>
+	<!-- END .header -->
+
+	<?php
+		require '../part_1.php';
+	 ?>
+	 <!-- name of building  -->
+	Hostel 2
+	<?php
+		require '../part_2.php';
+	?>
+	<!-- link of image and popup image -->
+	<a href="../../images_all_buildings/hostels/hostel_2.jpg" class="image-popup"><img src="../../images_all_buildings/hostels/hostel_2.jpg" alt="FREEHTML5.co Free HTML5 Template" class="img-responsive img-rounded"></a>
+	<?php
+		require '../part_3.php';
+	 ?>
+ 	<?php
+ 		require '../footer.php';
+ 	 ?>
+
+    <?php
+    	require '../script.php';
+     ?>
+	</body>
+</html>
---
+++ building/part_3.php
@@ -0,0 +1,19 @@
+</p>
+	<?php 
+		echo '<a href="kmz-files/' . $download . '" class="btn btn-primary btn-active btn-center" download>View in Google Earth!</a>';
+	 ?>
+						</div>
+                    </div> 
+                </div>
+	        </div>
+	 </div>
+</div>
+  
+<div id="fh5co-main">
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+
+
+ <div class="fh5co-spacer fh5co-spacer-lg"></div>
